dd32a33a26d286a50524c7a6f8c30dabfcdd7cd2
katelyna <katelyna@katelyna-HP>
2016-03-31 20:50:49 -0700

    Added main

--- tester/src/tester/tester.java
+++ tester/src/tester/tester.java
@@ -1,4 +1,14 @@
 package tester;
+/**
+ * Test class for first commit.
+ * 
+ * @author Kyle Lorenzano PIN: 517
+ *
+ * Edit showing ability to use Group GitHub @author Kevin Patterson PIN: 625
+ * 
+ * Edit showing ability to use Group GitHub @author Jacob Buchanan PIN: 203
+ * 
+ */
 
 public class tester {
 
